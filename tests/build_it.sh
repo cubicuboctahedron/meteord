3658d3f3fcf84a84a3ac266a974afcfff602ecb8
Ilya Grishkov <me@ilyagrishkov.com>
2016-08-15 21:19:33 +0200

    Remove binbuild

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 docker build -t zitemedia/meteord:base ../base
 docker build -t zitemedia/meteord:onbuild ../onbuild
-docker build -t zitemedia/meteord:devbuild ../devbuild
-docker build -t zitemedia/meteord:binbuild ../binbuild
