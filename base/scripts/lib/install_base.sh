4025049192b5b7878ec624f530bce884fd6aa833
Ilya Grishkov <me@ilyagrishkov.com>
2020-06-25 13:56:12 +0200

    Fix build
    - Add Debian Wheezy archive repos
    - Fix getting Meteor 1.4.0.1 from the official repo

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
-apt-get update -y
+echo "deb http://archive.debian.org/debian/ wheezy main contrib non-free 
+deb-src http://archive.debian.org/debian/ wheezy main contrib non-free
+deb http://archive.debian.org/debian-security/ wheezy/updates main non-free contrib
+deb-src http://archive.debian.org/debian-security/ wheezy/updates main non-free contrib" > /etc/apt/sources.list
+echo '---'
+cat /etc/apt/sources.list
+echo '---'
+apt-get update -y -o Acquire::Check-Valid-Until=false
 apt-get install -y curl bzip2 build-essential python git
--- base/scripts/lib/install_meteor.sh
+++ base/scripts/lib/install_meteor.sh
@@ -2,4 +2,4 @@ set -e
 
 RELEASE="1.4.0.1"
 
-curl -sL https://install.meteor.com | sed 's/RELEASE=.*/RELEASE="'$RELEASE'"/' | sed s/--progress-bar/-sL/g | /bin/sh
+curl -sL https://install.meteor.com?release=${RELEASE} | sed s/--progress-bar/-sL/g | /bin/sh
